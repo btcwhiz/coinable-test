6a184c27ff35ffac0e6f2c43c0eb41fc8b83e456
bestsilvertiger <stigerpanda@gmail.com>
2022-09-06 19:23:10 -0400

    add eslint rule for build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     domains: ['cdn.myanimelist.net'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
